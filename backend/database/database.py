1385033db954f028fe18995096ac897eb97e30c7
Luis <176695894+prodanyboy@users.noreply.github.com>
2026-02-24 20:38:18 -0600

    fix: update database engine configuration to use NullPool for MySQL connection

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
-# Reemplaza los datos con los que te dio Filess.io
-# Formato: mysql+pymysql://USUARIO:PASSWORD@HOST:PUERTO/NOMBRE_BD
 DATABASE_URL = "mysql+pymysql://Tienda_AGAL_lostelsewe:a70d5210da2322f4894a6a4864d04cc1a1128dad@0u6ka8.h.filess.io:3307/Tienda_AGAL_lostelsewe"
 
-# Quitamos el connect_args={"check_same_thread": False} porque eso solo era para SQLite
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
